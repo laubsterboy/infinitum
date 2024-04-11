5fefda05352f289646d0944e1d0b3309244934af
John Russell <john@johnrussell.dev>
2024-04-11 17:18:27 -0600

    Updates to the drawers.php class

--- inc/theme/drawers/drawers.php
+++ inc/theme/drawers/drawers.php
@@ -8,10 +8,11 @@
 
 namespace infinitum\inc\theme\drawers;
 
-class Drawers {
+class Drawers extends \infinitum\inc\classes\Addon {
     protected $drawer_post_type_slug = 'drawer';
-	protected $dir = '';
-	protected $uri = '';
+	protected readonly string $dir;
+	protected readonly array $new_drawer_defaults;
+	protected readonly string $uri;
 
     /**
 	 * Drawers construct
@@ -25,35 +26,69 @@ class Drawers {
 		$this->dir = $dir;
 		$this->uri = $uri;
 
+		$this->new_drawer_defaults = array(
+			'comment_status' => 'closed',
+			'ping_status' => 'closed',
+			'post_content' => '',
+			'post_status' => 'publish',
+			'post_type' => $this->get_post_type_slug()
+		);
+
 		$this->set_hooks();
 	}
 
 
 
 	/**
-	 * Gets the post ID assigned to a given drawer location
+	 * Create starter content so the default header drawer will have something to show
 	 * 
 	 * @since 0.0.1
-	 * @access public
 	 * 
-	 * @return boolean|integer
+	 * @return void
 	 */
-	public function get_drawer_id($location = '') {
-		$post_id = false;
+	protected function create_starter_content(): void {
+		$header_posts = $this->get_posts(array(
+			'name' => 'header'
+		));
 
-		if (!empty($location) && is_string($location)) {
-			if ($location === 'header') {
-				$post_id = 0; // TODO: Update this if need be, likely don't need this method at all
-			}
+		// If no "header" post is found then create the starter drawer
+		if (empty($header_posts)) {
+			wp_insert_post(wp_parse_args(array(
+				'post_content' => '<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","width":100,"widthUnit":"%","buttonText":"Search"} /--><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"horizontal"}} /-->',
+				'post_title' => 'Header'
+			), $this->new_drawer_defaults));
 		}
-
-		return $post_id;
 	}
 	
 
 
 	public function get_post_type_slug() {
 		return $this->drawer_post_type_slug;
+	}
+
+
+
+	/**
+	 * Gets the first published post created
+	 * 
+	 * @since 0.0.1
+	 * 
+	 * @return WP_Post|false
+	 */
+	public function get_first_post() {
+		$post = false;
+		$posts = $this->get_posts(array(
+			'numberposts' => 1,
+			'order' => 'ASC',
+			'orderby' => 'date',
+			'post_status' => 'publish'
+		));
+
+		if (is_array($posts) && !empty($posts)) {
+			$post = $posts[0];
+		}
+
+		return $post;
 	}
 
 
@@ -79,7 +114,7 @@ class Drawers {
         }
 
 		// Force the drawers post type
-        $args['post_type'] = $this->drawer_post_type_slug;
+        $args['post_type'] = $this->get_post_type_slug();
 
         return get_posts($args);
     }
@@ -105,7 +140,7 @@ class Drawers {
 	 * @return void
 	 */
     protected function register_post_type() {
-		register_post_type($this->drawer_post_type_slug, apply_filters('infinitum_custom_post_type_args', array(
+		register_post_type($this->get_post_type_slug(), apply_filters('infinitum_custom_post_type_args', array(
 			'labels' => array('name' => 'Drawers',
 				'singular_name' => 'Drawers',
 				'add_new' => 'Add New',
@@ -124,14 +159,14 @@ class Drawers {
 			'show_in_menu' => 'themes.php',
 			'show_in_nav_menus' => false,
 			'query_var' => true,
-			'capability_type' => 'post',
-			'hierarchical' => false,
+			'capability_type' => 'page',
+			'hierarchical' => true,
 			'show_in_rest' => true,
 			'supports' => array('title', 'editor', 'revisions'),
-			'taxonomies' => array('category', 'post_tag'),
+			'taxonomies' => array(),
 			'rewrite' => array('slug' => 'drawers', 'with_front' => false),
 			'menu_position' => 1,
-			'has_archive' => true), $this->drawer_post_type_slug)
+			'has_archive' => true), $this->get_post_type_slug())
 		);
 	}
 	
@@ -166,25 +201,40 @@ class Drawers {
 
 	public function render_drawer_block($block_attributes, $content) {
 		$drawer_id = 0;
-		$markup = '<div>Select a drawer</div>';
+		$drawer_post = null;
+		$content = 'Select a drawer';
+		$context = '';
+		$markup = '';
+		$wrapper = '<div ' . get_block_wrapper_attributes() . ' %s data-wp-interactive="infinitumDrawer">%s</div>';
+		$interactivity_data_context = array('isOpen' => false);
+
+		// Interactivity Data Context
+		if (function_exists('wp_interactivity_data_wp_context')) {
+			$context = wp_interactivity_data_wp_context($interactivity_data_context);
+		}
 
 		// Drawer ID
 		if (!empty($block_attributes['drawerID']) && is_numeric($block_attributes['drawerID'])) {
 			$drawer_id = intval($block_attributes['drawerID']);
 		}
 
-		if (!empty($drawer_id)) {
+		// Drawer Post
+		if ($drawer_id !== 0) {
 			$drawer_post = get_post($drawer_id);
-
-			if (is_a($drawer_post, '\WP_Post') && !empty($drawer_post->ID)) {
-				$markup = '<div class="infinitum-block-drawer">';
-				$markup .= $this->render_drawer_button_open($drawer_post, $block_attributes);
-				$markup .= $this->render_drawer_modal($drawer_post, $block_attributes);
-				$markup .= '</div>';
-			}
+		} else {
+			// Use the first Drawer post created, if another has not yet been selected
+			$drawer_post = $this->get_first_post();
 		}
 
-		return $markup;
+		// Render Drawer if $post is a WP_Post object
+		if (is_a($drawer_post, '\WP_Post') && !empty($drawer_post->ID)) {
+			$content = $this->render_drawer_button_open($drawer_post, $block_attributes);
+			$content .= $this->render_drawer_modal($drawer_post, $block_attributes);
+		} else {
+			$content = 'Drawer not found';
+		}
+
+		return sprintf($wrapper, $context, $content);
 	}
 
 
@@ -192,11 +242,11 @@ class Drawers {
 	protected function render_drawer_button_close($drawer_post, $block_attributes) {
 		$show_icon = !empty($block_attributes['showIconClose']) ? true : false;
 		$show_label = !empty($block_attributes['labelClose']) ? true : false;
-		$markup_icon__close = ($show_icon || (!$show_icon && !$show_label)) ? '<svg class="infinitum-block-drawer__button-icon infinitum-block-drawer__button-icon--close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path></svg>' : '';
+		$markup_icon__close = ($show_icon || (!$show_icon && !$show_label)) ? '<svg class="wp-block-infinitum-drawer__button-icon wp-block-infinitum-drawer__button-icon--close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path></svg>' : '';
 		$markup_aria_label = $show_label ? '' : ' aria-label="Open ' . $drawer_post->post_title . '"';
-		$markup_label__close = $show_label ? '<span class="infinitum-block-drawer__button-label infinitum-block-drawer__button-label--close">' . $block_attributes['labelClose'] . '</span>' : '';
+		$markup_label__close = $show_label ? '<span class="wp-block-infinitum-drawer__button-label wp-block-infinitum-drawer__button-label--close has-90-font-size">' . $block_attributes['labelClose'] . '</span>' : '';
 
-		return  '<button class="infinitum-block-drawer__button--close infinitum-block-drawer__button"' . $markup_aria_label . '>' . $markup_icon__close . $markup_label__close . '</button>';
+		return  '<button class="wp-block-infinitum-drawer__button--close wp-block-infinitum-drawer__button"' . $markup_aria_label . ' data-wp-on--click="actions.close">' . $markup_icon__close . $markup_label__close . '</button>';
 	}
 
 
@@ -204,11 +254,11 @@ class Drawers {
 	protected function render_drawer_button_open($drawer_post, $block_attributes) {
 		$show_icon = !empty($block_attributes['showIconOpen']) ? true : false;
 		$show_label = !empty($block_attributes['labelOpen']) ? true : false;
-		$markup_icon__open = ($show_icon || (!$show_icon && !$show_label)) ? '<svg class="infinitum-block-drawer__button-icon infinitum-block-drawer__button-icon--open" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="8" width="16" height="2"></rect><rect x="4" y="14" width="16" height="2"></rect></svg>' : '';
+		$markup_icon__open = ($show_icon || (!$show_icon && !$show_label)) ? '<svg class="wp-block-infinitum-drawer__button-icon wp-block-infinitum-drawer__button-icon--open" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="8" width="16" height="2"></rect><rect x="4" y="14" width="16" height="2"></rect></svg>' : '';
 		$markup_aria_label = $show_label ? '' : ' aria-label="Open ' . $drawer_post->post_title . '"';
-		$markup_label__open = $show_label ? '<span class="infinitum-block-drawer__button-label infinitum-block-drawer__button-label--open">' . $block_attributes['labelOpen'] . '</span>' : '';
+		$markup_label__open = $show_label ? '<span class="wp-block-infinitum-drawer__button-label wp-block-infinitum-drawer__button-label--open has-90-font-size">' . $block_attributes['labelOpen'] . '</span>' : '';
 
-		return  '<button class="infinitum-block-drawer__button--open infinitum-block-drawer__button"' . $markup_aria_label . '>' . $markup_icon__open . $markup_label__open . '</button>';
+		return  '<button class="wp-block-infinitum-drawer__button--open wp-block-infinitum-drawer__button"' . $markup_aria_label . ' data-wp-on--click="actions.open">' . $markup_icon__open . $markup_label__open . '</button>';
 	}
 
 
@@ -217,15 +267,15 @@ class Drawers {
 		/**
 		 * @see get_block_wrapper_attributes
 		 */
-		$attributes = wp_parse_args(\WP_Block_Supports::get_instance()->apply_block_supports(), array(
+		$attributes = array(
 			'class' => '',
 			'id' => '',
 			'style' => ''
-		));
+		);
 		$normalized_attributes = array();
 
 		// Custom classes
-		$attributes['class'] = 'infinitum-block-drawer__modal wp-block-infinitum-drawer has-global-padding is-layout-constrained ' . $attributes['class'];
+		$attributes['class'] = 'wp-block-infinitum-drawer__modal has-global-padding is-layout-constrained ' . $attributes['class'];
 
 		foreach ($attributes as $key => $value) {
 			if (!empty($value)) {
@@ -245,8 +295,8 @@ class Drawers {
 			}
 		}
 
-		$markup = '<aside ' . implode(' ', $normalized_attributes) . ' aria-label="' . $drawer_post->post_title . ' Modal" role="dialog">';
-		$markup .= '<div class="infinitum-block-drawer__button--close-container wp-block-group">' . $this->render_drawer_button_close($drawer_post, $block_attributes) . '</div>';
+		$markup = '<aside ' . implode(' ', $normalized_attributes) . ' data-wp-interactive="infinitumDrawer" aria-label="' . $drawer_post->post_title . ' Modal" role="dialog">';
+		$markup .= '<div class="wp-block-infinitum-drawer__button--close-container wp-block-group">' . $this->render_drawer_button_close($drawer_post, $block_attributes) . '</div>';
 		$markup .= $content;
 		$markup .= '</aside>';
 
@@ -258,6 +308,12 @@ class Drawers {
 	protected function set_hooks() {
 		add_action('init', array($this, 'wp_hook_init'));
 		add_filter('block_type_metadata_settings', array($this, 'wp_hook_block_type_metadata_settings'), 10, 2);
+	}
+
+
+
+	public function theme_activation($old_theme_name = null, $old_theme = null): void {
+		$this->create_starter_content();
 	}
 
 
